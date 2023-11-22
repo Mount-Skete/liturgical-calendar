41abda6289a951f4b7985030e36048503bc7e5d2
cosmas-mnt <150338877+cosmas-mnt@users.noreply.github.com>
2023-11-22 15:44:42 +0200

    Add testing dependencies

--- book/src/julian_calendar/__init__.py
+++ book/src/julian_calendar/__init__.py
@@ -1,5 +1,11 @@
-from .open_calendar import (julian_to_gregorian,
-                            gregorian_to_julian)
+from .open_calendar import (is_leap_gregorian_year,
+                            is_leap_julian_year,
+                            julian_to_gregorian,
+                            gregorian_to_julian,
+                            gregorian_to_julian_day,
+                            julian_to_julian_day,
+                            julian_day_to_gregorian,
+                            julian_day_to_julian)
 
 from .easter import (calculate_orthodox_easter_julian,
                      calculate_orthodox_easter_gregorian,
