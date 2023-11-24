86a44372d494f2a8988d9ce89937f32bc3f21b81
cosmas-mnt <150338877+cosmas-mnt@users.noreply.github.com>
2023-11-24 19:13:07 +0200

    Fix release workflow

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-git tag -s -a v0.0.1 -m "v0.0.1"
+for i in "$@"; do
+  case $i in
+    -v=*|--version=*)
+      VERSION="${i#*=}"
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $i"
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+done
+
+if [ "$VERSION" ]; then
+  echo "Releasing version $VERSION"
+  git tag -s -a "$VERSION" -m "$VERSION"
+else
+  echo "Usage: create-release.sh -v=v1.0.0"
+fi
